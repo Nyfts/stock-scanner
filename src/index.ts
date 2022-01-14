1ef92d3324f2bf0ae452bc2708354f578917bd1b
Luan Teixeira <luan.teixeira@dock.tech>
2022-01-14 17:33:37 -0300

    remoção tipo desnecessário

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import Product from './models/product';
 import NofitierService from './services/notifierService';
 import ProductService from './services/productService';
 import ChromiumService from './services/chromiumService';
 
 async function main() {
-  const products: Product[] = ProductService.listAllProducts();
+  const products = ProductService.listAllProducts();
 
   for (const product of products) {
     const productInfo = await ChromiumService.getProductInfo(product.url);
